5335f3ec93b542139398fd0bb2c6a8c78b4610c5
Mahdi Fayaznia <mahdifayaznia@gmail.com>
2025-12-28 23:29:28 +0330

    feat(prisma): setup Prisma ORM with SQLite

---
+++ _lib/prisma.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
+
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma =
+  globalForPrisma.prisma ??
+  (() => {
+    const connectionString = process.env.DATABASE_URL!;
+    const adapter = new PrismaBetterSqlite3({
+      url: connectionString,
+    });
+
+    return new PrismaClient({ adapter });
+  })();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
